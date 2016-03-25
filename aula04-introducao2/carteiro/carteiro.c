e152715ebf1f9f5ab6453d102c8c937725bae65f
Felipe Duarte <felipelageduarte@gmail.com>
2016-03-24 21:59:46 -0300

    aula 04

--- aula04-introducao2/carteiro/carteiro.c
+++ aula04-introducao2/carteiro/carteiro.c
@@ -2,6 +2,14 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]){
-    
+        
+    char a; // char -> 1 byte
+    int b;  // int -> 4 bytes
+    char c;
 
+    printf("end a: %p\n", &a);
+    printf("end b: %p\n", &b);
+    printf("end c: %p\n", &c);
+
+    return 0;
 }
